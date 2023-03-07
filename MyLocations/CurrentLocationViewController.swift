73f14812f2e83a694555d369183ee18498a6d51c
Kyle Musto <kylemusto1@gmail.com>
2023-03-06 20:12:42 -0500

    ch 31

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -160,24 +160,17 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
     }
     
     func string(from placemark: CLPlacemark) -> String {
-        var line1 = ""
-        if let tmp = placemark.subThoroughfare {
-            line1 += tmp + " "
-        }
-        if let tmp = placemark.thoroughfare {
-            line1 += tmp
-        }
-        var line2 = ""
-        if let tmp = placemark.locality {
-            line2 += tmp + " "
-        }
-        if let tmp = placemark.administrativeArea {
-            line2 += tmp + " "
-        }
-        if let tmp = placemark.postalCode {
-            line2 += tmp
-        }
-        return line1 + "\n" + line2
+      var line1 = ""
+      line1.add(text: placemark.subThoroughfare)
+      line1.add(text: placemark.thoroughfare, separatedBy: " ")
+
+      var line2 = ""
+      line2.add(text: placemark.locality)
+      line2.add(text: placemark.administrativeArea, separatedBy: " ")
+      line2.add(text: placemark.postalCode, separatedBy: " ")
+
+      line1.add(text: line2, separatedBy: "\n")
+      return line1
     }
     
     @objc func didTimeOut() {
---
+++ MyLocations/String+AddText.swift
@@ -0,0 +1,20 @@
+//
+//  String+AddText.swift
+//  MyLocations
+//
+//  Created by Kyle Musto on 3/6/23.
+//
+
+import Foundation
+
+extension String {
+    mutating func add(text: String?, separatedBy separator: String = "") {
+        if let text = text {
+            if !isEmpty {
+                self += separator
+            }
+            self += text
+        }
+    }
+}
+
